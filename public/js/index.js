757707c48ba142f1b5f911564d27b8e9e3678433
Ilya Zhidkov <zidkov.ilja@gmail.com>
2020-06-14 05:50:45 +0200

    Fix minor UX / UI issues

--- public/js/index.js
+++ public/js/index.js
@@ -2,5 +2,7 @@ const toggleButton = document.getElementById('toggle');
 const navigation = document.querySelector('header nav');
 
 toggleButton.addEventListener('click', event => {
+    event.preventDefault();
+    
     navigation.classList.toggle('expand');
 });
